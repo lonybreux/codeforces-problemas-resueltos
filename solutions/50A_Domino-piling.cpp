291b81a3de21638632757074490a19a21cf069ae
lonybreux <169080226+lonybreux@users.noreply.github.com>
2025-11-07 17:47:49 -0500

    50A_Domino-piling

---
+++ solutions/50A_Domino-piling.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int m, n;
+
+	cin >> m >> n;
+
+	cout << (m * n) / 2;
+
+	return 0;
+}
